6bc4468235a1135d063db9f0854e950d6c4b36b0
Ali Daw <alasdair.daw@gmail.com>
2016-10-25 17:13:39 +0100

    Add console app to call the actors lots.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Actor1.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appName = "fabric:/AliTestActors";
+            
+            while (true)
+            {
+                var actorId = ActorId.CreateRandom();
+                var actor = ActorProxy.Create<IActor1>(actorId, appName);
+                Console.WriteLine($"Starting job on actor {actorId}.");
+                actor.StartJob();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
