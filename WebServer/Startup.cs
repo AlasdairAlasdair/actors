04740847059a8a89c5bad3cf9c84ff420dbf714b
Ali Daw <alasdair.daw@gmail.com>
2016-10-26 14:34:49 +0100

    Add signal R (but nothing happens)

--- WebServer/Startup.cs
+++ WebServer/Startup.cs
@@ -29,6 +29,7 @@ namespace WebServer
         {
             // Add framework services.
             services.AddMvc();
+            services.AddSignalR(options => options.Hubs.EnableDetailedErrors = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,7 @@ namespace WebServer
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            app.UseSignalR();
             app.UseMvc();
         }
     }
