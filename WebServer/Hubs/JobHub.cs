69ef8526334b6346c43d4b69e4ad1c43f87149d0
Ali Daw <alasdair.daw@gmail.com>
2016-10-26 14:48:04 +0100

    Hook up signalR on connection to index.html

---
+++ WebServer/Hubs/JobHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebServer.Hubs
+{
+    public class JobHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
